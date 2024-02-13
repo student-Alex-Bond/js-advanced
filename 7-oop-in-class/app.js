cf75c4987dc0c3df2e319c2c9b7be295399ce53f
Alex_Bond <bondarenko.aleksandr.25@gmail.com>
2024-02-13 13:42:55 +0600

    поравил пробелы

--- 7-oop-in-class/app.js
+++ 7-oop-in-class/app.js
@@ -1,48 +1,46 @@
-class Person{
-    constructor(race, name, languange){
-        this.race = race,
-        this.name = name,
-        this.languange = languange
-    }
+class Person {
+  constructor(race, name, languange) {
+    (this.race = race), (this.name = name), (this.languange = languange);
+  }
 
-    say(){
-        console.log(this.languange, this.name);
-    }
+  say() {
+    console.log(this.languange, this.name);
+  }
 }
 
-class Ork extends Person{
-    constructor(name, weapon) {
-        super('ork', name, 'непонятный');
-        this.weapon = weapon;
-    }
-    doDamage() {
-        console.log('орк сделал удар -', this.weapon);
-    }
+class Ork extends Person {
+  constructor(name, weapon) {
+    super("ork", name, "непонятный");
+    this.weapon = weapon;
+  }
+  doDamage() {
+    console.log("орк сделал удар -", this.weapon);
+  }
 
-    say(){
-        console.log('Я орк');
-    }
+  say() {
+    console.log("Я орк");
+  }
 }
 
-class Elf extends Person{
-    constructor(name, spellType){
-        super('elf', name, 'старо непонятный');
-        this.spellType = spellType;
-    }
+class Elf extends Person {
+  constructor(name, spellType) {
+    super("elf", name, "старо непонятный");
+    this.spellType = spellType;
+  }
 
-    createSpell(){
-        console.log('эльф сделал заклинание', this.spellType);
-    }
+  createSpell() {
+    console.log("эльф сделал заклинание", this.spellType);
+  }
 
-    say() {
-        console.log('я эльф');
-    }
+  say() {
+    console.log("я эльф");
+  }
 }
 
-const avgustin = new Ork('Avgustin', 'клюка');
+const avgustin = new Ork("Avgustin", "клюка");
 avgustin.say();
 avgustin.doDamage();
 
-const valentin = new Elf('Valentin', 'fire wave');
+const valentin = new Elf("Valentin", "fire wave");
 valentin.say();
 valentin.createSpell();
